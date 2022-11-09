edf660ca9776f211dac5558f704d3df5055c5c1d
SifEddine05 <sifousamir72@gmail.com>
2022-11-09 01:34:11 +0100

    resolve problems

--- ui_ux_website/src/pages /busniess.js
+++ ui_ux_website/src/pages /busniess.js
@@ -15,25 +15,25 @@ const Busniess = ()=>{
                     <button className="rounded bg-[#33BBCF]  mt-2 md:mt-4 sm:mt-3 lg:mt-5  text-[8px] sm:text-[10px] md:text-[14px] lg:text-[18px]  w-[40px] sm:w-[80px] md:w-[120px] lg:w-[160px] h-[28px] sm:h-[38px] md:h-[48px] lg:h-[58px] drop-shadow-xl hover:text-white" >Get Started</button>
                 </div>
                 <div className="flex flex-col justify-around items-start w-[35%]">
-                    <div className="flex justify-around items-center">
-                        <div className="rounded-full bg-[#09977C] bg-opacity-25 w-[23%] h-[20px] md:h-[40px] sm:h-[30px] lg:h-[64px] ">
-                            <img src={Star} alt="star img" className=' w-[50%] h-[50%] mt-[25%] ml-[25%]'/>   
+                    <div className="flex justify-around items-center ">
+                        <div className="rounded-full bg-[#09977C] bg-opacity-25 w-[20px] md:w-[40px] sm:w-[30px] lg:w-[64px] h-[20px] md:h-[40px] sm:h-[30px] lg:h-[64px] ">
+                            <img src={Star} alt="star img" className=' w-[50%] h-[50%] m-[25%]'/>   
                         </div>
-                        <div className='ml-2 md:ml-4 sm:ml-3 lg:ml-5'>
-                            <h3 className='text-[6px] sm:text-[10px] md:text-[14px] lg:text-[18px] text-[#FFFF] '>
+                        <div className='ml-2 md:ml-4 sm:ml-3 lg:ml-5 w-[80%]'>
+                            <h3 className=' text-[6px] sm:text-[10px] md:text-[14px] lg:text-[18px] text-[#FFFF] '>
                                 Rewards 
                             </h3>
-                            <p className='text-[6px] sm:text-[10px] md:text-[14px] lg:text-[18px] text-[#FFFF] opacity-75'>
+                            <p className=' text-[6px] sm:text-[10px] md:text-[14px] lg:text-[18px] text-[#FFFF] opacity-75'>
                                 The best credit cards offer some tantalizing combinations of promotions and prizes
                             </p>
                         </div>
                     </div>
 
                     <div className="flex justify-around items-center  mt-4 md:mt-8 sm:mt-6 lg:mt-10 bg-[#42475B] rounded-lg  bg-opacity-75  h-30px sm:h-[60px] md:h-[80px] lg:h-[100px]">
-                        <div className="rounded-full bg-[#09977C] bg-opacity-25 w-[23%] h-[20px] md:h-[40px] sm:h-[30px] lg:h-[64px] ml-1 drop-shadow-[0_20px_100px_[#42475B]">
-                          <img src={Sheild} alt="star img" className=' w-[50%] h-[50%] mt-[25%] ml-[25%]'/>
+                        <div className="rounded-full bg-[#09977C] bg-opacity-25  w-[20px] md:w-[40px] sm:w-[30px] lg:w-[64px] h-[20px] md:h-[40px] sm:h-[30px] lg:h-[64px]  ml-1 drop-shadow-[0_20px_100px_[#42475B]">
+                          <img src={Sheild} alt="star img" className=' w-[50%] h-[50%] m-[25%] '/>
                         </div>
-                        <div className='ml-2 md:ml-4 sm:ml-3 lg:ml-5'>
+                        <div className='ml-2 md:ml-4 sm:ml-3 lg:ml-5 w-[80%]'>
                             <h3 className='text-[8px] sm:text-[10px] md:text-[14px] lg:text-[18px] text-[#FFFF] '>
                             100% Secured 
                             </h3>
@@ -43,10 +43,10 @@ const Busniess = ()=>{
                     </div>
 
                     <div className="flex justify-around items-center mt-4 md:mt-8 sm:mt-6 lg:mt-10 ">
-                        <div className="rounded-full bg-[#09977C] bg-opacity-25 w-[23%] h-[20px] md:h-[40px] sm:h-[30px] lg:h-[64px] ">
-                          <img src={Send} alt="star img" className=' w-[50%] h-[50%] mt-[25%] ml-[25%]'/>
+                        <div className="rounded-full bg-[#09977C] bg-opacity-25 w-[20px] md:w-[40px] sm:w-[30px] lg:w-[64px] h-[20px] md:h-[40px] sm:h-[30px] lg:h-[64px] ">
+                          <img src={Send} alt="star img" className=' w-[50%] h-[50%] m-[25%]'/>
                         </div>
-                        <div className='ml-2 md:ml-4 sm:ml-3 lg:ml-5'>
+                        <div className='ml-2 md:ml-4 sm:ml-3 lg:ml-5 w-[80%]'>
                             <h3 className='text-[8px] sm:text-[10px] md:text-[14px] lg:text-[18px] text-[#FFFF] '>
                                 Balance Transfer 
                             </h3>
--- ui_ux_website/src/pages /navBar.js
+++ ui_ux_website/src/pages /navBar.js
@@ -2,10 +2,11 @@
 import {  useState } from 'react';
 import logo from '../assets/logo.svg' ;
 import menu from '../assets/menu.svg' ;
-
 const NavBar= ()=> {
     const [style , setStyle] = useState(false) ; 
+    
 const handlClick =()=>{
+
     if(style===false)
     {
         setStyle(true);
@@ -18,7 +19,7 @@ const handlClick =()=>{
       <nav className=" w-full flex-col justify-center items-center sm:flex sm:flex-row sm:justify-between sm:items-center" >
           <div className='w-full flex justify-center sm:inline'>
                 <img src={logo} alt="logo"  className='w-[124px] h-[32px]  sm: ml-[9.88%] '/>
-                <button className=' ml-1  sm:hidden  ' onClick={handlClick} ><img src={menu} alt="menu icon " /></button>
+                <button className=' ml-1  sm:hidden  ' onClick={handlClick}  ><img src={menu} alt="menu icon " /></button>
           </div>
             
             <ul className="hidden sm:flex   flex-end mr-[10%]">
@@ -27,14 +28,14 @@ const handlClick =()=>{
                 <li className='mr-[10%] flex items-center ' ><a href='#Features' className='no-underline text-white hover:text-[#33BBCF]'>Features</a></li>
                 <li className='  mr-[10%] flex items-center'><a href='#Solution' className='no-underline text-white hover:text-[#33BBCF]'>Solution</a></li>
             </ul>
-           {style && <ul className=' flex flex-col justify-center items-center sm:hidden '>
-                <li className='mr-[10%] hover:bg-white hover:bg-opacity-25 ' ><a href='#home' className='no-underline  text-white hover:text-[#33BBCF]'>Home</a></li>
-                <hr/>
-                <li className='mr-[10%] hover:bg-white hover:bg-opacity-25'><a href='#AboutUs' className='no-underline text-white hover:text-[#33BBCF]'>AboutUs</a></li>
-                <li className='mr-[10%]  hover:bg-white hover:bg-opacity-25' ><a href='#Features' className='no-underline text-white hover:text-[#33BBCF]'>Features</a></li>
-                <li className='  mr-[10%] hover:bg-white hover:bg-opacity-25'><a href='#Solution' className='no-underline text-white hover:text-[#33BBCF]'>Solution</a></li>
+            {style && <ul className=' block m-auto sm:hidden '>
+                <li className=' hover:bg-white hover:bg-opacity-25 ' ><a href='#home' className='no-underline ml-[50%] text-white hover:text-[#33BBCF]'>Home</a></li>
+                <li className=' hover:bg-white hover:bg-opacity-25'><a href='#AboutUs' className='no-underline ml-[50%] text-white hover:text-[#33BBCF]'>AboutUs</a></li>
+                <li className='  hover:bg-white hover:bg-opacity-25' ><a href='#Features' className='no-underline  ml-[50%] text-white hover:text-[#33BBCF]'>Features</a></li>
+                <li className='   hover:bg-white hover:bg-opacity-25'><a href='#Solution' className='no-underline  ml-[50%] text-white hover:text-[#33BBCF]'>Solution</a></li>
             </ul>}
         </nav>
+         
 
     ) ; 
 } 
